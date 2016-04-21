0952bacd76e0c0089677790c63c99bf4136b3413
cuixingchen <cuipengfei@yeah.net>
2016-04-21 16:14:02 +0800

    成功了快 (cherry picked from commit b80d4dc)

--- netty-server/src/main/java/com/cui/netty_server/msg/MessageID.java
+++ netty-server/src/main/java/com/cui/netty_server/msg/MessageID.java
@@ -5,13 +5,14 @@ package com.cui.netty_server.msg;
  * Reason:	 TODO ADD REASON. 
  * Date:     2014年10月15日 下午4:58:32 
  * @author   cuipengfei
- * 删除
+ * 删除123
  */
 public class MessageID {
 	/** 链路登录请求消息*/
 	public static final int UP_CONNECT_REQ = 0x2001;
 	/** 链路登录应答消息*/
 	public static final int UP_CONNECT_RSP = 0x2002;
+	private int i;
 
 }
 
